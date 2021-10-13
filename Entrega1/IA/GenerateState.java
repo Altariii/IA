3344e46ab01ea56d6b0074308d4ffbdc32b2a3b4
Albert Tarrés <atarres2001@hotmail.com>
2021-10-13 17:31:33 +0200

    Entrega 1

--- Entrega1/IA/GenerateState.java
+++ Entrega1/IA/GenerateState.java
@@ -1,3 +1,5 @@
+package IA;
+
 public class GenerateState {
 
 }
---
+++ Entrega1/Main.java
@@ -0,0 +1,20 @@
+import Libraries.Gasolina;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random seed = new Random();
+        int GAS_MAX = 100;
+        // NUM_GAS Luego se cambia por args[0]
+        int NUM_GAS = 20;
+
+        Gasolineras new_gasolineras = new Gasolineras(NUM_GAS, seed.nextInt(GAS_MAX));
+        for(int i = 0; i < new_gasolineras.size(); i++) {
+            Gasolinera g = new_gasolineras.get(i);
+            System.out.println(g.getCoordX());
+        }
+    }
+
+}
