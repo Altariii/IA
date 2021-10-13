c6f55be3a291963be599f619f1a1ed3eb8ec28a6
Albert Tarrés <atarres2001@hotmail.com>
2021-10-13 18:08:12 +0200

    Entrega 1

--- Entrega1/IA/GenerateState.java
+++ Entrega1/IA/GenerateState.java
@@ -1,5 +1,14 @@
 package IA;
 
+import IA.Gasolina.*;
+
 public class GenerateState {
 
+    Gasolineras gas;
+    CentrosDistribucion cd;
+
+    public GenerateState(Gasolineras gas, CentrosDistribucion cd) {
+        this.gas = gas;
+        this.cd = cd;
+    }
 }
