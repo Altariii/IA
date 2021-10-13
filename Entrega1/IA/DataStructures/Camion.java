d0d7e5f05d7e26df656a2587f3040df36b25afbd
Albert Tarrés <atarres2001@hotmail.com>
2021-10-14 00:18:32 +0200

    Clase camion

--- Entrega1/IA/DataStructures/Camion.java
+++ Entrega1/IA/DataStructures/Camion.java
@@ -1,12 +1,14 @@
 package IA.DataStructures;
 
+import aima.util.Pair;
 import java.util.ArrayList;
 
 public class Camion {
     int num_viajes;
     int kilometros;
     int depositocamion;
-    ArrayList<Integer> Peticiones;
+    int Peticion;
+    Pair posicion;
 
     public int getNum_viajes() {
         return num_viajes;
@@ -20,15 +22,40 @@ public class Camion {
         return depositocamion;
     }
 
-    public ArrayList<Integer> getPeticiones() {
-        return Peticiones;
+    public int getPeticion() {
+        return Peticion;
     }
 
-    public Camion(int num_viajes, int kilometros, int depositocamion, ArrayList<Integer> Peticiones) {
+    public Pair getPosicion() {
+        return posicion;
+    }
+
+    public void setNum_viajes(int num_viajes) {
+        this.num_viajes = num_viajes;
+    }
+
+    public void setKilometros(int kilometros) {
+        this.kilometros = kilometros;
+    }
+
+    public void setDepositocamion(int depositocamion) {
+        this.depositocamion = depositocamion;
+    }
+
+    public void setPeticion(int Peticion) {
+        this.Peticion = Peticion;
+    }
+
+    public void setPosicion(Pair posicion) {
+        this.posicion = posicion;
+    }
+
+    public Camion(int num_viajes, int kilometros, int depositocamion, int Peticion, Pair posicion) {
         this.num_viajes = num_viajes;
         this.kilometros = kilometros;
         this.depositocamion = depositocamion;
-        this.Peticiones = Peticiones;
+        this.Peticion = Peticion;
+        this.posicion = posicion;
     }
 
 }
