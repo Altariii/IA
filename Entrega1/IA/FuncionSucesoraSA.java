9282ae7ccbc4c352739ec134cd2c54a15ae3e7bd
Altariii <atarres2001@gmail.com>
2021-10-20 13:56:45 +0200

    Clase FuncionSucesoraSA

---
+++ Entrega1/IA/FuncionSucesoraSA.java
@@ -0,0 +1,73 @@
+package IA;
+
+import aima.search.framework.SuccessorFunction;
+import aima.search.framework.Successor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class FuncionSucesoraSA implements SuccessorFunction {
+
+    public List getSuccessors(Object state) {
+        ArrayList sucesor = new ArrayList();
+        Estado estado = (Estado) state;
+
+        /*
+         Si getRandomNum == 0, Operador ADD
+         si == 1, Operador SwapPeticionNoAtendida
+         si == 2, Operador SwapPeticionesCamiones
+         */
+
+        int RandLowerBound = 0;
+        int RandUpperBound = 2;
+        boolean sucesorViable = false;
+
+        while (sucesorViable == false) {
+
+            // Solo se puede usar swapViajes
+            if (estado.getPeticiones().size() == 0) {
+                RandLowerBound = 2;
+            }
+            // Estado vacio -> Solo se puede usar add
+            else if (estado.getNumPeticiones() == estado.getPeticiones().size()) {
+                RandUpperBound = 0;
+            }
+
+            int randomNum = getRandomNum(RandLowerBound, RandUpperBound);
+            int numCamion = getRandomNum(0, estado.getCamiones().size() - 1);
+            int numPeticionCamion = getRandomNum(0, estado.getCamiones().get(numCamion).getViajesCamion().size() - 1);
+            int numPeticionNoAtendida = getRandomNum(0, estado.getPeticiones().size() - 1);
+
+            Estado estado_sucesor = estado.clona_estado();
+
+            switch (randomNum) {
+                case 0:
+                    // Add
+                    if (estado_sucesor.addPeticion_Camion(numCamion, numPeticion)) {
+                        sucesorViable = true;
+                        sucesor.add(new Successor("Centro " + (numCamion+1) + ", nueva peticion" + (numPeticion+1) + " Beneficio: " + estado.getTotalBeneficio(), estado_sucesor));
+                    }
+                    break;
+
+                case 1:
+                    // SwapPeticionNoAtendida
+                    if (estado_sucesor.swapPeticionNoAtendida(numCamion, numPeticionCamion, numPeticionNoAtendida)) {
+                        sucesorViable = true;
+                        sucesor.add(new Successor("Peticion " + (numPeticionCamion+1) + " del camion" + (numCamion+1) + " cambiada por peticion no atendida" + (numPeticionNoAtendida+1), estado_sucesor));
+                    }
+                    break;
+
+                case 2:
+                    // SwapPeticionesCamiones
+                    //int numCamion2 = getRandomNum(0, )
+                    break;
+            }
+        }
+        return sucesor;
+    }
+
+    private int getRandomNum(int LowerBound, int UpperBound) {
+        Random num = new Random();
+        return num.nextInt((UpperBound - LowerBound) + 1) + LowerBound;
+    }
+}
