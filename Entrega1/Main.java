56ccdc59076f979da6aff266f61f60c1316742f4
Sergio <sergio.perez.moliner@estudiantat.upc.edu>
2021-10-23 14:54:11 +0200

    Clase Main

--- Entrega1/Main.java
+++ Entrega1/Main.java
@@ -1,15 +1,65 @@
-import java.util.Random;
+import java.util.*;
 import IA.Gasolina.*;
-import IA.Estado;
+import IA.*;
+import aima.search.framework.Problem;
+import aima.search.framework.Search;
+import  aima.search.framework.SearchAgent;
+import aima.search.informed.HillClimbingSearch;
+import aima.search.informed.SimulatedAnnealingSearch;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        //PARAMETROS
         Random num = new Random();
         int seed = num.nextInt(100);
-        CentrosDistribucion centros = new CentrosDistribucion(5, 1, seed);
-        Gasolineras gasolineras = new Gasolineras(10, seed);
+        int num_cd = 10;
+        int num_gas = 10;
+        boolean hill_climbing = true;
+        CentrosDistribucion centros = new CentrosDistribucion(num_cd, 1, seed);
+        Gasolineras gasolineras = new Gasolineras(num_gas, seed);
+
+        //ESTADO INICIAL
         Estado inicial = new Estado(centros, gasolineras, true);
-        inicial.print_camiones();
+        Problem problema;
+        Search algoritmo;
+
+        if (hill_climbing) {
+            problema = new Problem(inicial, new FuncionSucesoraHC(), new IAGoalTest(), new FuncionHeurisitica());
+            algoritmo = new HillClimbingSearch();
+        }
+
+        else {
+            problema = new Problem(inicial, new FuncionSucesoraSA(), new IAGoalTest(), new FuncionHeurisitica());
+            algoritmo = new SimulatedAnnealingSearch(1000,10,100,0.01);
+        }
+
+        double tiempo = System.currentTimeMillis();
+
+        SearchAgent agent = new SearchAgent(problema,algoritmo);
+
+
+        //RESULTADOS
+        if (hill_climbing) pinta_acciones(agent.getActions());
+        pinta_Instrumentacion(agent.getInstrumentation());
+        System.out.println("Tiempo de ejecucion: " + (System.currentTimeMillis() - tiempo) + "ms");
+    }
+
+    //FUNCIONES PRIVADAS
+    private static void pinta_acciones(List acciones) {
+        for (int i = 0; i < acciones.size(); ++i) {
+            String accion = (String) acciones.get(i);
+            System.out.println(accion);
+        }
+    }
+
+    private static void pinta_Instrumentacion (Properties propiedades) {
+        Iterator keys = propiedades.keySet().iterator();
+        while (keys.hasNext()) {
+            String key = (String) keys.next();
+            String propiedad = propiedades.getProperty(key);
+            System.out.println(key + " : " + propiedad);
+        }
     }
 }
